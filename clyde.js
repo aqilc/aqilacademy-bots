4a7ba2c71e0c5196ebce2cb3f30934fe748d85c0
Glitch (clyde3) <none>
2018-04-22 14:56:36 +0000

    🌘🐆 Checkpoint ./clyde.js:649699/425

--- clyde.js
+++ clyde.js
@@ -15,11 +15,9 @@ app.listen(process.env.PORT);
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function(){
-  if(!exists){
-    db.run("CREATE TABLE IF NOT EXISTS users (id TEXT, points INTEGER, lastDaily INTEGER)");
-    db.run("CREATE TABLE IF NOT EXISTS elections (num INTEGER PRIMARY KEY, start INTEGER, end INTEGER, going TEXT, winner TEXT)");
-    db.run("CREATE TABLE IF NOT EXISTS modlogs (
-  } else {
-    
-  }
+  db.run("CREATE TABLE IF NOT EXISTS users (id TEXT, points INTEGER, lastDaily INTEGER)");
+  db.run("CREATE TABLE IF NOT EXISTS elections (num INTEGER PRIMARY KEY, start INTEGER, end INTEGER, going TEXT, winner TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS warns (num INTEGER PRIMARY KEY, warn TEXT, mod TEXT, dat INTEGER)");
+  db.run("CREATE TABLE IF NOT EXISTS items (name TEXT, price TEXT, user TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS expstore (item TEXT, price TEXT, approv
 });
