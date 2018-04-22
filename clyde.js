aeda23c5515670b0eeffebe4d897ec1b268aac4f
Glitch (clyde3) <none>
2018-04-22 22:02:34 +0000

    👬🍧 Checkpoint ./clyde.js:649699/1294 ./election.json:649699/47

--- clyde.js
+++ clyde.js
@@ -8,12 +8,6 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./.data/sqlite.db');
 const Discord = require("discord.js");
 
-//Election database
-const low = require("lowdb");
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync("election.json");
-const elec = low(adapter);
-
 //The client
 var client = new Discord.Client({ autoreconnect: true });
 
@@ -34,6 +28,7 @@ db.serialize(function(){
   db.run("CREATE TABLE IF NOT EXISTS warns (num INTEGER PRIMARY KEY, warn TEXT, mod TEXT, date INTEGER)");
   db.run("CREATE TABLE IF NOT EXISTS items (name TEXT, price TEXT, user TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS expstore (num INTEGER PRIMARY KEY, item TEXT, desc TEXT, stock INTEGER, price INTEGER, approved TEXT, bought TEXT, seller TEXT, buyer TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS elections (num INTEGER PRIMARY KEY, winner TEXT, end INTEGER, start INTEGER, vp TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS election (num INTEGER PRIMARY KEY, id TEXT, vId TEXT, votes INTEGER, msgId TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS voters (id TEXT, for TEXT, date INTEGER)");
   
@@ -41,15 +36,19 @@ db.serialize(function(){
 
 var f = {
   checkelections: () => {
-    if(elec.end > new Date().valueOf()) {
-      setTimeout(() => {
-        let winners = "";
-        for(var i = 0; i < elec.candidates.length; i++) {
-          elec.candidates
-        }
-        client.guilds.get("294115797326888961").channels.get(chnls.a).send(`**:yes: The election has officially ended. Winner(s):**\`\`\`\n\`\`\``);
-      }, elec.end - new Date().valueOf());
-    }
+    db.all("SELECT * FROM elections", (err, res) => {
+      if(res[res.length-1].end > new Date().valueOf()) {
+        let winner = "";
+        setTimeout(() => {
+          db.run("SELECT * FROM election", (err, res) => {
+            for(var i of res) {
+              if(i.votes
+            }
+            client.guilds.get("294115797326888961").channels.get(chnls.a).send(`**:yes: The election has officially ended. Winner(s):**\`\`\`\n\`\`\``);
+          })
+        }, res[length-1].end - new Date().valueOf());
+      }
+    });
   }
 };
 
