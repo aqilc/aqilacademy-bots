c7a2e63a6628a78a3d484b4011e7e9dd72cb3ec2
Glitch (clyde3) <none>
2018-04-29 22:02:23 +0000

    ⏰🚧 Checkpoint ./clyde.js:649699/34

--- clyde.js
+++ clyde.js
@@ -148,7 +148,7 @@ var f = {
     db.get(`SELECT * FROM users WHERE id = "${id}"`, (err, res) => {
       if(!res)
         return db.run(`INSERT INTO users (id, points, realpoints, messages, created) VALUES (?, ?, ?, ?, ?)`, [id, 0, 0, 1, new Date().valueOf()]) && console.log("Created user: " + id);
-      db.run(`UPDATE users SET messages = ${res.messages + 1}, points = ${res.points + xp}, realpoints = ${res.realPoints + xp} WHERE id = "${id}"`);
+      db.run(`UPDATE users SET messages = ${res.messages + 1}, points = ${res.points + xp}, realpoints = ${res.realpoints + xp} WHERE id = "${id}"`);
     });
   },
   random: (min, max, round) => {
@@ -195,13 +195,13 @@ const cmds = {
     del: false,
     do: (msg, content) => {
       if(content !== "") {
-        if(cmds[content.toLowerCase()]) {
+        if(cmds[content.toLowerCase()])
           return msg.channel.send(new Discord.RichEmbed().setAuthor(content.toLowerCase(), client.user.avatarURL).setDescription(cmds[content.toLowerCase()].desc).addField("Usage", `\`\`\`\n${content.toLowerCase() + cmds[content.toLowerCase()].usage}\`\`\``).addField("Permissions", ["admin", "mod"].includes(cmds[content.toLowerCase()].perms) ? `You need the \`${{ mod: "Moderator", admin: "Administrator" }[cmds[content.toLowerCase()].perms]} role to use this command` : { "bot admin": "You need to be a Bot Administrator to use this command", "admin perm": "You need the `ADMINISTRATOR` permission to use this command", "": "You do not need ANY permissions to use this command" }[cmds[content.toLowerCase()].perms]).addField("Category", `${cmds[content.toLowerCase()].cat} (${prefix}help ${cmds[content.toLowerCase()].cat})`));
-        } else if(["utility", "bot admin", "exp", "election", "fun"].includes(content.toLowerCase()))
+        else if(["utility", "bot admin", "exp", "election", "fun"].includes(content.toLowerCase()))
           return msg.channel.send(new Discord.RichEmbed().setAuthor(content.slice(0, 1).toUpperCase() + content.slice(1, content.length) + " Commands", client.user.avatarURL).setDescription({ fun: "A bunch of fun commands you can just play around with!", utility: "Commands used for modding, and commands that also give us some info about the bot. ", "bot admin": "A bunch of commands only the Clyde Administrators(people who coded Clyde) can use", exp: "These commands let you interact with Clyde EXP and the EXP Store.", election: "Commands you can use to interact with the AqilAcademy Elections!" }[content.toLowerCase()]).addField("Commands", (function(cat) { let cmd = ""; for(let i in cmds) { if(cmds[i].cat === cat) return " "; } return ""})(content.toLowerCase()) === "" ? "No commands yet" : (function(cat) { let cmd = ""; for(let i in cmds) { if(cmds[i].cat === cat) cmd += `**${prefix + i + cmds[i].usage}:** ${cmds[i].desc}\n`; } return cmd})(content.toLowerCase())).setColor(f.color()));
         else
           return msg.channel.send(new Discord.RichEmbed().setAuthor(`Error: No command or category named "${content}" found.`, client.user.avatarURL).setColor(f.color()).setFooter(`Do "${prefix}help" to see all commands and categories.`));
-        return console.log(`"${content}"`);
+        return console.log(`help error: "${content}"`);
       }
       let embed = new Discord.RichEmbed()
         .setColor(f.color())
