4bfbaddefd73a1d1faa1d3a8c1681c12a6214b28
Glitch (clyde3) <none>
2018-05-06 17:52:28 +0000

    🎹🚦 Checkpoint ./clyde.js:649699/352

--- clyde.js
+++ clyde.js
@@ -201,6 +201,17 @@ var f = {
     }
     return has;
   },
+  imagedata_to_image: (imagedata) => {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = imagedata.width;
+    canvas.height = imagedata.height;
+    ctx.putImageData(imagedata, 0, 0);
+
+    var image = new Image();
+    image.src = canvas.toDataURL();
+    return image;
+  }
 };
 
 // Events
