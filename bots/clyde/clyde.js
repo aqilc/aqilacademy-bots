e25659c7972082ebd6f911d807aa599eb7573c95
Glitch (aqilacademy-bots) <none>
2018-07-16 17:37:59 +0000

    🏵📠 Checkpoint ./bots/clyde/clyde.js:649699/234

--- bots/clyde/clyde.js
+++ bots/clyde/clyde.js
@@ -693,6 +693,10 @@ const cmds = {
         db.run(`DELETE FROM waiting WHERE id = 0`);
         db.run("DELETE FROM election");
         msg.guild.channels.get(data.echnl).overwritePermissions(msg.guild.roles.get("294115797326888961"), { READ_MESSAGES: false });
+        msg.guild.members.array().forEach(m => {
+          if(m.roles.get(msg.guild.find("name", "Candidate").id))
+            m.roles.get(mdg.find("name", "Candidate").id).
+        });
         client.channels.get(data.echnl).send(new Discord.RichEmbed().setAuthor("Election has officially stopped by " + msg.author.tag, msg.author.avatarURL).setDescription("There might have been technical problems so please don't be angry").setColor(f.color()));
         msg.reply("Ended Election #" + res[res.length-1].num)
       });
