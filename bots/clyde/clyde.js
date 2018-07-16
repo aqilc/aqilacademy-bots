ac7c1457808559511ee10529444b14c6b4b5d58b
Glitch (aqilacademy-bots) <none>
2018-07-16 21:21:09 +0000

    🎽🚗 Checkpoint ./package.json:649699/2312 ./bots/clyde/clyde.js:649699/276

--- bots/clyde/clyde.js
+++ bots/clyde/clyde.js
@@ -11,6 +11,8 @@ const Discord = require("discord.js");
 const data = require("/app/data/cd.js");
 const levels = require("/app/data/l.js");
 const globalfunctions = require("/app/data/f.js");
+const cheerio = require("cheerio");
+const querystring = require("querystring");
 
 // Prefix
 const prefix = "c.";
@@ -1000,24 +1002,22 @@ const cmds = {
           f: async function(mess) {
             let searchMessage = await msg.reply('Searching... Sec.');
             let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
-
-            // We will now use snekfetch to crawl Google.com. Snekfetch uses promises so we will
-            // utilize that for our try/catch block.
             return snekfetch.get(searchUrl).then((result) => {
 
-            // Cheerio lets us parse the HTML on our google result to grab the URL.
-            let $ = cheerio.load(result.text);
+              // Cheerio lets us parse the HTML on our google result to grab the URL.
+              let $ = cheerio.load(result.text);
 
-            // This is allowing us to grab the URL from within the instance of the page (HTML)
-            let googleData = $('.r').first().find('a').first().attr('href');
+              // This is allowing us to grab the URL from within the instance of the page (HTML)
+              let googleData = $('.r').first().find('a').first().attr('href');
 
-            // Now that we have our data from Google, we can send it to the channel.
-            googleData = querystring.parse(googleData.replace('/url?', ''));
-            searchMessage.edit(`Result found!\n${googleData.q}`);
+              // Now that we have our data from Google, we can send it to the channel.
+              googleData = querystring.parse(googleData.replace('/url?', ''));
+              searchMessage.edit(`Result found!\n${googleData.q}`);
 
             // If no results are found, we catch it and return 'No results are found!'
             }).catch((err) => {
-               searchMessage.edit('No results found!');
+              console.log(err);
+              searchMessage.edit('No results found!');
             });
           }
         }
