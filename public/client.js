722bc77a5c4210733808e71a11aabce5d370f4df
Glitch (aqilacademy-bots) <none>
2018-07-19 16:53:02 +0000

    💺🚆 Checkpoint ./public/client.js:649699/77

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-// Make unnecessary distractions go away XD
+// Makes unnecessary distractions go away XD
 /* global
  * Envirnment
   createCanvas, windowWidth, windowHeight, width, height, mouseX, mouseY, 
@@ -12,7 +12,7 @@
   push, pop,
  * Shapes
   ellipse, rect
-*/
+ */
 
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
@@ -21,13 +21,13 @@ function draw() {
   background(255);
   rectMode(CENTER);
   fill(0);
-  ellipse(mouseX, mouseY, 20, 20);
+  ellipse(mouseX, mouseY, 200, 200);
   
   push();
   noFill();
+  strokeWeight(3);
   for(let i = 0; i < 10; i ++) {
-    stroke(255, (10 - i) * 255);
-    strokeWeight(4);
+    stroke(255, ((10 - i)/10) * 255);
     rect(width/2, height/2, width - i, height - i, (i/10) * 5);
   }
   pop();
