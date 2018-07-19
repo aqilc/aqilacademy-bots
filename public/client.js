ba4a3b6be747a9d25e20516ef2047a85ea333601
Glitch (aqilacademy-bots) <none>
2018-07-19 16:42:08 +0000

    🌘🙌 Checkpoint ./public/client.js:649699/573 ./public/style.css:649699/76

--- public/client.js
+++ public/client.js
@@ -1,20 +1,34 @@
-/* global createCanvas, windowWidth, windowHeight, background, mouseIsPressed, fill, mouseX, mouseY, ellipse, rect */
+// Make unnecessary distractions go away XD
+/* global
+ * Envirnment
+  createCanvas, windowWidth, windowHeight, width, height, mouseX, mouseY, 
+ * Events
+  mouseIsPressed, 
+ * Modes/Aligns
+  rectMode, ellipseMode, CENTER, CORNER, TOP, BOTTOM, LEFT, RIGHT
+ * Color
+  fill, stroke, strokeWeight, noFill, background, 
+ * Transformations
+  push, pop,
+ * Shapes
+  ellipse, rect
+*/
 
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
 }
 function draw() {
-  background(0);
+  background(255);
   rectMode(CENTER);
   fill(0);
   ellipse(mouseX, mouseY, 20, 20);
   
   push();
   noFill();
-  stroke(255, 255, 255, 50);
-  for(let i = -1; i < 10; i ++) {
-    strokeWeight(10);
-    rect(width/2, height/2, width - i, height - i);
+  for(let i = 0; i < 10; i ++) {
+    stroke(255, (10 - i) * 255);
+    strokeWeight(4);
+    rect(width/2, height/2, width - i, height - i, (i/10) * 5);
   }
   pop();
 }
