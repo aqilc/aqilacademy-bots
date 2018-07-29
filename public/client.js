0de256c5ad49b13de03c2c7c77bd7b1104046e8b
Glitch (aqilacademy-bots) <none>
2018-07-29 02:28:43 +0000

    🚁🍇 Checkpoint ./public/client.js:649699/330

--- public/client.js
+++ public/client.js
@@ -26,32 +26,36 @@ Array.prototype.width = function (arr) {
 };
 
 const hr = {
-  edituser(obj) {
-    if(!obj)
-      throw new Error("No editing parameters specified");
-    
-    let validParams = ["id", "points", "realpoints"], params = "", id = "";
-    if(obj.id) {
-      id = obj.id;
-      delete obj.id;
-    } else
-      throw new Error("ID not provided when changing the 'users' database");
-    
-    if(obj === {})
-      throw new Error("Nothing to edit");
-    
-    params += `id=${id}`;
-    for(let i in obj) {
-      if(!validParams.includes(i))
-        throw new Error("Invalid User Database Change");
-      
-      params += `&${i}=${obj[i]}`;
-    }
-    
-    let req = new XMLHttpRequest();
-    req.open("POST", "/db/run/users?" + params);
-    req.send();
+  set: {
+    user(obj) {
+      if(!obj)
+        throw new Error("No editing parameters specified");
+
+      let validParams = ["id", "points", "realpoints"], params = "", id = "";
+      if(obj.id) {
+        id = obj.id;
+        delete obj.id;
+      } else
+        throw new Error("ID not provided when changing the 'users' database");
+
+      if(obj === {})
+        throw new Error("Nothing to edit");
+
+      params += `id=${id}`;
+      for(let i in obj) {
+        if(!validParams.includes(i))
+          throw new Error("Invalid User Database Change");
+
+        params += `&${i}=${obj[i]}`;
+      }
+
+      let req = new XMLHttpRequest();
+      req.open("PUT", "/db/run/users?" + params);
+      req.send();
+    },
   },
+  get: {},
+  create: {},
 };
 
 function Text(txt, x, y, s = 6, w, h, type = 0) {
