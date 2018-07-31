4979201ba26b76ccb5384413a66dbfd6e89814ac
Glitch (aqilacademy-bots) <none>
2018-07-31 01:59:46 +0000

    🏰💏 Checkpoint ./public/data/f.js:649699/890

--- public/data/f.js
+++ public/data/f.js
@@ -15,37 +15,3 @@
  * Shapes
   ellipse, rect
  */
-
-function Text(txt, x, y, s = 6, w, h, type = 0) {
-  if(typeof txt !== "string")
-    txt = JSON.stringify(txt);
-  
-  let font = fonts[type || 0], tx = x, ty = y;
-  if(textAlign().horizontal === "center") {
-    let tw = (txt.split("").length - 1) * s;
-    txt.split("\n").width(true).split("").forEach(c => {
-      if(!font[c])
-        return;
-      tw += font[c].width() * s;
-    });
-    tx -= tw/2;
-  }
-  if(textAlign().vertical === "center")
-    ty -= txt.split("\n").length * s * 3.5;
-  for(let g = 0; g < txt.length; g ++) {
-    let i = txt[g];
-    if(i === "\n") {
-      tx = x, ty += s * 7;
-      continue;
-    }
-    if(!font[i])
-      continue;
-    for(let j = 0; j < font[i].length; j ++) {
-      for(let h = 0; h < font[i][j].length; h ++) {
-        if(font[i][j][h] === "1")
-          rect(tx + h * s - h, ty + j * s, s, s);
-      }
-    }
-    tx += font[i].width() * s + s;
-  }
-}
