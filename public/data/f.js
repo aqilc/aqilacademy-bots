5d5e2c5542b0d1465129c24dbabaf82f83a4cecf
Glitch (aqilacademy-bots) <none>
2018-07-31 23:02:15 +0000

    ⛹☃️ Checkpoint ./public/data/f.js:649699/531

--- public/data/f.js
+++ public/data/f.js
@@ -1,6 +1,11 @@
 class Button {
   constructor(w, h, colors = [255, 0]) {
+    let args = ["w", "h", "colors"];
+    for(let i = 0; i < arguments.length; i ++) {
+      this[args[i > args.length ? args.length : i]] = arguments[i];
+    }
     
+    this.pxart = [];
   }
   draw(txt, x, y) {
     
