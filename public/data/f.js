04012cbefa53fd608d1b01cbd5368160f324c8f7
Glitch (aqilacademy-bots) <none>
2018-07-31 23:23:30 +0000

    💈🤒 Checkpoint ./public/data/f.js:649699/312

--- public/data/f.js
+++ public/data/f.js
@@ -1,13 +1,17 @@
 class Button {
-  constructor(w, h, colors = [255, 0], options = { border: 2, px: 2 }) {
-    let args = ["w", "h", "colors"];
+  constructor(w, h, cols = [255, 0], opt = { border: 2, px: 2 }) {
+    w = Math.round(w / opt.px) * opt.px;
+    h = Math.round(h / opt.px) * opt.px;
+    let args = ["w", "h", "cols", "opt"];
     for(let i = 0; i < arguments.length; i ++) {
       this[args[i > args.length ? args.length : i]] = arguments[i];
     }
     
     this.pxart = [];
-    for(let i = 0; i < w/options.px; i ++) {
-      
+    for(let i = 0; i < w/opt.px; i ++) {
+      for(let j = 1; j < h/opt.px; j ++) {
+        
+      }
     }
   }
   draw(txt, x, y) {
