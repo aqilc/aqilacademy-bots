acf69e861705563b003ec6257a912266f118405c
Glitch (aqilacademy-bots) <none>
2018-07-31 23:13:10 +0000

    🤘🚒 Checkpoint ./public/data/f.js:649699/168

--- public/data/f.js
+++ public/data/f.js
@@ -1,11 +1,14 @@
 class Button {
-  constructor(w, h, colors = [255, 0]) {
+  constructor(w, h, colors = [255, 0], options = { border: 2, px: 2 }) {
     let args = ["w", "h", "colors"];
     for(let i = 0; i < arguments.length; i ++) {
       this[args[i > args.length ? args.length : i]] = arguments[i];
     }
     
     this.pxart = [];
+    for(let i = 0; i < w/options.px; i ++) {
+      
+    }
   }
   draw(txt, x, y) {
     
