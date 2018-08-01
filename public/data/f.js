2f2fff2039b5096044258207be90fb50e48766d5
Glitch (aqilacademy-bots) <none>
2018-08-01 21:33:30 +0000

    👆💨 Checkpoint ./public/data/f.js:649699/622

--- public/data/f.js
+++ public/data/f.js
@@ -15,20 +15,22 @@ class Button {
     this.cols = cols;
     this.opt = opt;
     
-    this.pxart = [];
-    for(let i = 1; i < w/opt.px - 1; i ++) {
-      this.pxart[0].push("1");
-      this.pxart[h/opt.px].push("1");
-    }
+    this.pxart = [[""]];
+    this.pxart[h/opt.px] = [""];
+    for(let i = 1; i < w/opt.px - 1; i ++)
+      this.pxart[0].push("1"), this.pxart[h/opt.px].push("1");
     if(opt.border > 1) {
-      
+      for(let i = 0; i < opt.border - 1; i ++) {
+        [this.pxart[i], this.pxart[h/opt.px - i]] = [];
+        for(let j = 0; j < w/opt.px; j ++)
+          this.pxart[i].push("1"), this.pxart[h/opt.px - i].push(1);
+      }
     }
     for(let i = opt.border; i < h/opt.px - opt.border; i ++) {
-      this.pxart[i] = ["1"];
-      for(let j = opt.border; j < w/opt.px - opt.border; j ++) {
-        this.pxart[i].push("2")
-      }
-      this.pxart[i].push("1");
+      this.pxart[i] = "1".repeat(opt.border).split("");
+      for(let j = opt.border; j < w/opt.px - opt.border; j ++)
+        this.pxart[i].push("2");
+      "1".repeat(opt.border).split("").forEach(v => this.pxart[i].push(v));
     }
     
   }
