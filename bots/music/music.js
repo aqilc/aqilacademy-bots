c195e85ccff93bc5effa49b63a89dba56ad35002
Glitch (aqilacademy-bots) <none>
2018-08-31 01:08:37 +0000

    💦🗻 Checkpoint ./bots/music/music.js:649699/426

--- bots/music/music.js
+++ bots/music/music.js
@@ -28,7 +28,7 @@ function run() {
     // Lets the admins run code through the bot
     if(msg.content.startsWith("a:")) {
       let snips = [
-        () => msg.channel.send("<:loadinggif:406945578967367680>"),
+        "process.exit(0)",
       ];
       
       if(!data.devs.includes(msg.author.id))
@@ -165,7 +165,7 @@ const c = {
     async f(msg, content) {
       
       // Defines variables
-      let vid, ms, downloaded, start = Date.now(), desc = d => `**File Size:** \`${vid.size || 0} bytes(${gFuncs.bytes(vid.size || 0)})\`\n**Length:** ${vid.length_seconds} seconds(${gFuncs.time(vid.length_seconds * 1000)})\n**Completed:** \`${d || 0} bytes(${gFuncs.bytes(d || 0)})\`\n**Time Taken:** ${Date.now() - start} ms(${gFuncs.time(Date.now() - start)})`, i;
+      let vid, ms, downloaded, start = Date.now(), desc = d => `[**Link to video**](${vid.video_url})\n**File Size:** \`${vid.size || 0} bytes(${gFuncs.bytes(vid.size || 0)})\`\n**Length:** ${vid.length_seconds} seconds(${gFuncs.time(vid.length_seconds * 1000)})\n**Completed:** \`${d || 0} bytes(${gFuncs.bytes(d || 0)})\`\n**Time Taken:** ${Date.now() - start} ms(${gFuncs.time(Date.now() - start)})`, i;
       
       // Starts typing to indicate that its working
       msg.channel.startTyping();
@@ -183,8 +183,8 @@ const c = {
       let stream = yt(vid.video_url, { filter : 'audioonly' });
       
       // Sends a message to indicate that the video is being downloaded then edits it every 3 seconds
-      ms = await msg.channel.send(new Discord.RichEmbed().addField(`<:loadinggif:406945578967367680> Downloading [${vid.title}](${vid.video_url})`, desc(vid.downloaded)).setThumbnail(vid.thumbnail_url))
-      i = setInterval(() => ms.edit(new Discord.RichEmbed().addField(`<:loadinggif:406945578967367680> Downloading [${vid.title}](${vid.video_url})`, desc(vid.downloaded)).setThumbnail(vid.thumbnail_url)), 3000);
+      ms = await msg.channel.send(new Discord.RichEmbed().addField(`<a:loadinggif:406945578967367680> Downloading [${vid.title}](${vid.video_url})`, desc(vid.downloaded)).setThumbnail(vid.thumbnail_url))
+      i = setInterval(() => ms.edit(new Discord.RichEmbed().addField(`<a:loadinggif:406945578967367680> Downloading ${vid.title}`, desc(vid.downloaded)).setThumbnail(vid.thumbnail_url)), 3000);
       
       // Video Information recieved when downloading
       stream.on('progress', function(chunk, down, total) {
