c62b698888eaf09bc3db0a7ba4e5c484902cfced
Glitch (aqilacademy-bots) <none>
2018-09-09 23:20:15 +0000

    🎊📺 Checkpoint ./data/d.js:649699/748

--- data/d.js
+++ data/d.js
@@ -10,6 +10,6 @@ let data = {
   echnl: "410233209201229854",
   
   // Videos to pick from at random
-  vids: [],
+  vids: ["_p5exp2TZeM", "8-gpAw17vhc", "U4hMQtcKfQo", "wijlEgFZFAk", "s35RIQu-9HM", "WAC7ec3z9uw", "bH4OeB5cSfM", "ejGZu1uZk94", "GNfmtgQ-PBE", "qkPC2vYRSdQ", "n_wliH1dVNE", "O3Z7hmRtGq4", "KZSUjlYFGmQ", "nmsWnYiTVT4", "3MkJ3TyP0uM", "AKkKeNtYMTo", "xft9wOGs8Us", "y91hSD1csl4", "mKyjzA9PA1A", "tfcZ5BHKsQk", "lHKuPOlq9c8", "DJFHfNZCADc", "T6oBzaXk6Zk", "TTGiW3oEtMI", "JJ7hVHPFl4I", "mPerS_QhVLI", "DksYAfaoXBs", "QS8w2WM39IQ", "bQSXuWsVGcQ", "gfR2cvDPC10", "WUJK8jQghI4", "THc7PbVHGaQ", "LhwQqwJAays", "NhImHFdB-vE", "OYSroM6UYso", "OEU3t0KMhrU", "qpChrUKFhx8", "2m07K8WTcSo", "LXyoQ4l_v6k", "pkML3i8tV_Y", "93vjfDKhf2o", "dfvd03GHMBU", "IjUcbWg4SEo", "XtsvWk53KJk", "UO8TQqheSBM", "RAe1I5ZBOGQ", "X78KKpVCf1E", "66hlb1cOmnA", "tS3HPXCxJUw", "P_84rA1KupM"],
 };
 module.exports = data;
