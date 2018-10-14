5ad29052aa588d59853ae2df5700384f0409644a
Glitch (aqilacademy-bots) <none>
2018-10-14 18:04:26 +0000

    🍙💸 Checkpoint ./bots/clyde/clyde.js:649699/155 ./main.js:649699/2 ./watch.json:649699/2

--- bots/clyde/clyde.js
+++ bots/clyde/clyde.js
@@ -95,6 +95,10 @@ function run() {
     // Resets streaks of those who haven't collected today
     setTimeout(f.reset_streaks, new Date().setHours(11, 59, 59).valueOf() - new Date().valueOf());
   });
+  client.on("guildMemberAdd", member => {
+    if(member.username.includes('discord.gg/'))
+      member.ban();
+  })
   client.on("guildMemberRemove", member => {
     db.all(`SELECT * FROM election WHERE id = "${member.user.id}", vId = "${member.user.id}"`, (err, res) => {
       if(!res)
